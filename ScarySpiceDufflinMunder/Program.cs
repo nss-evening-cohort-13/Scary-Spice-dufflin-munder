14cec580360675eac1e96812962ca166547b9066
Jordan Smith <jordansmith989@gmail.com>
2021-02-08 19:31:14 -0600

    setup

---
+++ ScarySpiceDufflinMunder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScarySpiceDufflinMunder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
