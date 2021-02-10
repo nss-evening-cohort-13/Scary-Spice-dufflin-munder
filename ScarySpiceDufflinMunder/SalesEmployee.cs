72d5924dab6ccd0c052d76a060427cffd6b00af9
Chris Calhoun <ctcalhoun2@gmail.com>
2021-02-09 18:09:18 -0600

    started SalesEmployee class.

---
+++ ScarySpiceDufflinMunder/SalesEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScarySpiceDufflinMunder
+{
+    class SalesEmployee
+    {
+        public List<Client> Clients { get; set; } = new List<Client>();
+        public List<Sale> Sales { get; set; } = new List<Sale>();
+
+        public void AddClient (Client client)
+        {
+            Clients.Add(client);
+        }
+        public void AddSale(Sale sale)
+        {
+            Sales.Add(sale);
+        }
+
+    }
+}
