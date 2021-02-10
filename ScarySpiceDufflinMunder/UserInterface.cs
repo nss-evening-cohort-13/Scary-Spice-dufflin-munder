f88f849eb6407a929bb461a04fc32e3df89a406b
Jordan Smith <jordansmith989@gmail.com>
2021-02-09 18:51:09 -0600

    main switch statement finished

---
+++ ScarySpiceDufflinMunder/UserInterface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScarySpiceDufflinMunder
+{
+    class UserInterface
+    {
+        
+        public static void MainGreeting()
+        {
+            Console.WriteLine(@"
+Welcome to Dufflin/Munder Cardboard Co. 
+Sales Portal!
+
+1.Enter Sales
+2.Generate Report For Accountant
+3.Add New Sales Employee
+4.Find a Sale
+5.Exit
+                     ");
+        }
+        public static void UserSelection(int caseSwitch)
+        {
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Case 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Case 5");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+        }
+    }
+}
